97a3abc043414c0ee6bface4da33e59e3d078583
Omar Boquiño Navarro <zs23013990@estudiantes.uv.mx>
2026-07-25 01:05:27 -0600

    refactor: extraer validacion en SensorRegistry
    se realizo un par de  comentarios para un mejor entendimiento.

--- semana2/MIERCOLES/test_TDD_estricto.py
+++ semana2/MIERCOLES/test_TDD_estricto.py
@@ -1,7 +1,7 @@
+#importamos librerias 
 import pytest
 from registry import SensorRegistry, SensorNotFoundError
-
-
+#se deja igual al codigo original 
 def test_get_unknown_sensor_raises():
     registry = SensorRegistry()
     with pytest.raises(SensorNotFoundError):
