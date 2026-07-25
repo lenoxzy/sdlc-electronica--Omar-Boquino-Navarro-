c75652d3caccc2b19d559efef352d4a469c0e9e0
lenoxzy <zs23013990@estudiantes.uv.mx>
2026-07-25 00:59:09 -0600

    refactor: se realizo un una mejora

--- semana2/MIERCOLES/registry.py
+++ semana2/MIERCOLES/registry.py
@@ -1,9 +1,13 @@
-# registry.py
 
 class SensorNotFoundError(Exception):
     pass
-
+# diccionario para guardar los sensores 
 class SensorRegistry:
+    def __init__(self):
+        self._sensors = {}
+
     def get(self, sensor_id: str):
-        # El código mínimo para que pase el test
-        raise SensorNotFoundError(f"El sensor {sensor_id} no fue encontrado.")
+        # Verificamos si realmente existe en el diccionario
+        if sensor_id not in self._sensors:
+            raise SensorNotFoundError(f"El sensor {sensor_id} no fue encontrado.")
+        return self._sensors[sensor_id]
