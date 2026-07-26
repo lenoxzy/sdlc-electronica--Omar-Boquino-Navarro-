259931f02387f5ff6100c0f954b9f221bf13ef19
lenoxzy <zs23013990@estudiantes.uv.mx>
2026-07-25 21:26:28 -0600

    test: especificar sensor  (RED) - us-01

---
+++ semana2/VIERNES/eval1/test_US-01.py
@@ -0,0 +1,8 @@
+# test_monitor.py
+import pytest
+# Test US-01: Ingesta de datos
+def test_sensor_reading_creation():
+    reading = SensorReading(sensor_id="TEMP-01", value=36.5, reading_type="temperature")
+    assert reading.sensor_id == "TEMP-01"
+    assert reading.value == 36.5
+    assert reading.reading_type == "temperature"
