b194cca430fcb0cd43341ad11b925e6da6a83342
lenoxzy <zs23013990@estudiantes.uv.mx>
2026-07-25 23:33:21 -0600

    FIX:correccion de errores de bibliotecas (US-04)

---
+++ semana2/VIERNES/eval1/US_4.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from Umbral import  AlertStrategy
+# --- Implementaciones Concretas de la Alerta ---
+class ConsoleAlert:
+    def send_alert(self, message: str) -> None:
+        print(f"[CONSOLE ALERT] {message}")
+
+class FileAlert:
+    def __init__(self, filepath: str):
+        self._filepath = filepath
+
+    def send_alert(self, message: str) -> None:
+        with open(self._filepath, "a", encoding="utf-8") as f:
+            f.write(f"[FILE ALERT] {message}\n")
