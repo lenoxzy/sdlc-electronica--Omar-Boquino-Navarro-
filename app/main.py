f44770b033c6473342a505bf4a279ca7092fa263
lenoxzy <zs23013990@estudiantes.uv.mx>
2026-08-08 10:08:33 -0600

    chore: bump version a 0.1.1 para demostrar CD

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from app.routers import reading_router, sensor_router
 
-app = FastAPI(title="SensorHub API", version="0.1.0")
+app = FastAPI(title="SensorHub API", version="0.1.1") # cambio de version 
 
 app.include_router(sensor_router.router)
 app.include_router(reading_router.router)
